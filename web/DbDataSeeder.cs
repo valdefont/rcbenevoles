5e2dc873a13fb647041d9708d771457958b6fd61
reality77 <davmail08-misc@yahoo.fr>
2017-11-20 23:47:18 +0100

    Initialising web app

---
+++ web/DbDataSeeder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web
+{
+    public static class DbDataSeeder
+    {
+        // Astuce pour appeler une méthode de SeedData
+        // TODO: Move this code when seed data is implemented in EF 7
+
+        public static void SeedData(this IServiceScopeFactory scopeFactory)
+        {
+            using (var serviceScope = scopeFactory.CreateScope())
+            {
+                var dbcontext = serviceScope.ServiceProvider.GetService<dal.RCBenevoleContext>();
+                dbcontext.SeedData();
+            }
+        }
+    }
+}
