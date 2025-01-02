505add2c9fc793576ff80d162f93f12b8f4c44d7
jordicuchet <jordi_cuchet@hotmail.com>
2025-01-02 21:38:13 +0100

    set log file count

--- web/Program.cs
+++ web/Program.cs
@@ -21,7 +21,7 @@ namespace web
             .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
             .Build();
 
-        public static int Main(string[] args)
+        /*public static int Main(string[] args)
         {
             var loggerConfig = new LoggerConfiguration()
                 .MinimumLevel.Debug()
@@ -48,6 +48,51 @@ namespace web
                 Log.Information("Starting web host");
                 var host = CreateHostBuilder(args).Build();
                 
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }*/
+
+        public static int Main(string[] args)
+        {
+            var loggerConfig = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", new LoggingLevelSwitch(Serilog.Events.LogEventLevel.Information))
+                .MinimumLevel.Override("System", new LoggingLevelSwitch(Serilog.Events.LogEventLevel.Information))
+                .WriteTo.Console();
+
+            var fileLogPath = Environment.GetEnvironmentVariable("APP_LOG_FILE_PATH");
+            int fileLogFileCount = int.TryParse(Environment.GetEnvironmentVariable("APP_LOG_FILE_COUNT"), out var count) ? count : 15; // Default to 15 if not set
+
+            if (!string.IsNullOrEmpty(fileLogPath))
+            {
+                loggerConfig = loggerConfig.WriteTo.File(
+                    fileLogPath,
+                    rollingInterval: RollingInterval.Day,
+                    outputTemplate: "{Timestamp:o} [{Level:u4}] {Message:lj}{NewLine}{Exception}",
+                    retainedFileCountLimit: fileLogFileCount > 0 ? (int?)fileLogFileCount : null
+                );
+            }
+
+            Log.Logger = loggerConfig
+                .Enrich.FromLogContext()
+                .Enrich.WithThreadId()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host with log file path: {FileLogPath} and file count limit: {FileLogFileCount}", fileLogPath, fileLogFileCount);
+                var host = CreateHostBuilder(args).Build();
+
                 host.Run();
                 return 0;
             }
